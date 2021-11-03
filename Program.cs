27e4719c880e0ffb44c7ebed0c684fe58794a307
Mateusz <mszymanski027@gmail.com>
2021-11-03 01:07:13 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UniversitySecondYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
