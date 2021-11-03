20d2c56815b984f7af67e3f978a65730adfa5c71
Mateusz <mszymanski027@gmail.com>
2021-11-03 01:13:29 +0100

    add

---
+++ Adapter/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace WzorzecAdapter
+{
+    //KOD Z ZEWNĘTRZNEJ BIBLIOTEKI
+    public class UsersApi
+    {
+        public async Task<string> GetUsersXmlAsync()
+        {
+            var apiResponse =
+                "<?xml version= \"1.0\" encoding= \"UTF-8\"?><users><user name=\"John\" surname=\"Doe\"/><user name=\"John\" surname=\"Wayne\"/><user name=\"John\" surname=\"Rambo\"/></users>";
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(apiResponse);
+
+            return await Task.FromResult(doc.InnerXml);
+        }
+    }
+
+    public class MyApi
+    {
+        public string GetUsersCsv()
+        {
+            List<string> listA = new List<string>();
+            using (var reader = new StreamReader(@"users.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    listA.Add(values[0]);
+                }
+            }
+
+            return string.Join(" ", listA);
+        }
+    }
+
+
+    public interface IUserRepository
+    {
+        List<string> GetUserNames();
+    }
+
+    public class UsersRepositoryAdapter : IUserRepository
+    {
+        private UsersApi _adaptee = null;
+
+        public UsersRepositoryAdapter(UsersApi adaptee)
+        {
+            _adaptee = adaptee;
+        }
+
+        public List<string> GetUserNames()
+        {
+            string incompatibleApiResponse = this._adaptee
+                .GetUsersXmlAsync()
+                .GetAwaiter()
+                .GetResult();
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(incompatibleApiResponse);
+
+            var rootEl = doc.LastChild;
+
+            List<string> userNames = new List<string>();
+
+            if (rootEl.HasChildNodes)
+            {
+                foreach (XmlNode user in rootEl.ChildNodes)
+                {
+                    userNames.Add(user.Attributes["name"].InnerText + user.Attributes["surname"].InnerText);
+                }
+            }
+
+            return userNames;
+        }
+    }
+
+    public class MyUserRepositoryAdapter : IUserRepository
+    {
+        private MyApi _adaptee = null;
+
+        public MyUserRepositoryAdapter(MyApi adaptee)
+        {
+            _adaptee = adaptee;
+        }
+
+        public List<string> GetUserNames()
+        {
+            return _adaptee.GetUsersCsv().Split(' ').ToList();
+        }
+    }
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyApi userRepository = new MyApi();
+
+            IUserRepository adapter = new MyUserRepositoryAdapter(userRepository);
+
+            List<string> users = adapter.GetUserNames();
+            var starNum = 1;
+            foreach (var userName in users)
+            {
+                Console.WriteLine($"{starNum},{userName.Replace(",", ",")}");
+                starNum++;
+            }
+        }
+    }
+}
---
+++ Prototyp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class ProductPrototype
+{
+
+    public decimal Price;
+
+    public ProductPrototype(decimal price)
+    {
+        Price = price;
+    }
+
+    public ProductPrototype Clone()
+    {
+        return (ProductPrototype)this.MemberwiseClone();
+    }
+}
+
+public class Bread : ProductPrototype
+{
+    public Bread(decimal price) : base(price) { }
+}
+
+public class Butter : ProductPrototype
+{
+
+    public Butter(decimal price) : base(price) { }
+}
+
+public class Supermarket
+{
+
+    private Dictionary<string, ProductPrototype> _productList = new Dictionary<string, ProductPrototype>();
+
+    public void AddProduct(string key, ProductPrototype productPrototype)
+    {
+        _productList.Add(key, productPrototype);
+    }
+
+    public ProductPrototype GetClonedProduct(string key)
+    {
+        ProductPrototype product = _productList[key];
+        ProductPrototype clonedPrototype = product.Clone();
+
+        return clonedPrototype;
+    }
+
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Supermarket supermarket = new Supermarket();
+        supermarket.AddProduct("Bread", new Butter(9.50m));
+        supermarket.AddProduct("Butter", new Butter(5.30m));
+
+        ProductPrototype product_discount = supermarket.GetClonedProduct("Bread");
+        Console.WriteLine(String.Format("Bread - {0} zl > {1}", product_discount.Price, product_discount.Price * 0.9m));
+        Console.WriteLine();
+
+        ProductPrototype product = supermarket.GetClonedProduct("Bread");
+        Console.WriteLine(String.Format("Bread - {0} zl", product.Price));
+        Console.WriteLine();
+
+        ProductPrototype productButter = supermarket.GetClonedProduct("Butter");
+        Console.WriteLine(String.Format("Butter - {0} zl", productButter.Price));
+        Console.WriteLine();
+    }
+}
