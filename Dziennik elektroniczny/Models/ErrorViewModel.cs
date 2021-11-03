55d8cba28f9b575580c3578a6625b276279c80ea
Mateusz <mszymanski027@gmail.com>
2021-11-03 01:24:06 +0100

    add

---
+++ Dziennik elektroniczny/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dziennik_elektroniczny.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Laboratoria/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Laboratoria.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
