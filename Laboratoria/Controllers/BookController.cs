ae6a0a13721b92c753e662a9dfc17088d6df458e
Mateusz <mszymanski027@gmail.com>
2021-11-05 09:34:10 +0100

    asp.net lab the end

--- Laboratoria/Controllers/BookController.cs
+++ Laboratoria/Controllers/BookController.cs
@@ -1,15 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Laboratoria.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Laboratoria.Models;
 
-namespace Laboratoria.Controllers
+namespace ASP_laby.Controllers
 {
     public class BookController : Controller
     {
         static List<Book> books = new List<Book>();
+        private static int index = 0;
         public IActionResult Index()
         {
             return View(books);
@@ -18,17 +19,58 @@ namespace Laboratoria.Controllers
         {
             return View();
         }
-
+        [HttpPost]
+        public IActionResult Add(Book Book)
+        {
+            if (ModelState.IsValid)
+            {
+                index++;
+                Book.Id = index;
+                books.Add(Book);
+                return View("ConfirmBook", Book);
+            }
+            else
+                return View("AddForm");
+        }
         public IActionResult List()
         {
             return View(books);
         }
-
-        [HttpPost]
-        public IActionResult Add(Book book)
+        public IActionResult EditForm(int id)
         {
-            books.Add(book);
-            return View("ConfirmBook", book);
+            return View(FindBook(id));
+        }
+        [HttpPost]
+        public IActionResult Edit(Book editedBook)
+        {
+            if (ModelState.IsValid)
+            {
+                Book originalBook = FindBook(editedBook.Id);
+                originalBook.Author = editedBook.Author;
+                originalBook.Title = editedBook.Title;
+                originalBook.PublishingYear = editedBook.PublishingYear;
+                return View("List", books);
+            }
+            else
+                return View("EditForm");
+        }
+        public IActionResult DeleteConfirm(int id)
+        {
+            return View(FindBook(id));
+        }
+        public IActionResult Delete(int id)
+        {
+            books.Remove(FindBook(id));
+            return View("List", books);
+        }
+        public Book FindBook(int id)
+        {
+            foreach (var book in books)
+            {
+                if (book.Id == id)
+                    return book;
+            }
+            throw new Exception("Nie ma takiej książki");
         }
     }
 }
--- Laboratoria/Models/Book.cs
+++ Laboratoria/Models/Book.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,14 @@ namespace Laboratoria.Models
 {
     public class Book
     {
+        [Required(ErrorMessage = "Musisz podać tytuł książki!")]
+        [MinLength(2)]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Musisz podoać autora!")]
         public string Author { get; set; }
-        public int PublicationYear { get; set; }
+        [Range(minimum: 2000, maximum: 2030, ErrorMessage = "Rok wydania musi być pomiędzy 2000 a 2030")]
+        public int PublishingYear { get; set; }
+        [HiddenInput]
+        public int Id { get; set; }
     }
 }
